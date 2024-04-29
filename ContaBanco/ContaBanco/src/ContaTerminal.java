4d3a84c631c78141cb4b35ff8dff2630ce3d7100
Roni Braga <sousaronielson@gmail.com>
2024-04-29 19:45:53 -0300

    corrigindo a mensagem final para o usuario, especificamente a palavra disponivel.

--- ContaBanco/ContaBanco/src/ContaTerminal.java
+++ ContaBanco/ContaBanco/src/ContaTerminal.java
@@ -20,7 +20,7 @@ public class ContaTerminal {
         System.out.println(
                 "Olá " + cliente + " obrigado por criar uma conta em nosso banco, sua agência é " + agencia
                         + " , conta "
-                        + numero + " e seu saldo " + saldo + " já está disonível para saque ");
+                        + numero + " e seu saldo " + saldo + " já está disponível para saque ");
 
     }
 }
